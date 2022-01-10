5ee0dbbee4d2e495346bf0fc59232bf7ce1369fd
TeacherTalley <ptalley2@uca.edu>
2022-01-10 13:46:43 -0600

    Starting the project

---
+++ arrays.cpp
@@ -0,0 +1,15 @@
+//
+// arrays.cpp 
+// This is a code base to use for the first git lab.
+//
+
+#include <iostream>
+using namespace std;
+
+void printArray(int n, int arr[])
+{
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << endl;
+	}
+}
+
